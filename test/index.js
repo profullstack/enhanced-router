b570dbef3d36f44a7127f5ce9b352e85d94134f5
Anthony Ettinger <anthony@chovy.com>
2025-05-11 23:36:21 -0700

    release: @profullstack/enhanced-router@0.2.5

--- test/index.js
+++ test/index.js
@@ -2,48 +2,27 @@
  * Basic tests for @profullstack/enhanced-router
  */
 
-// Import the module
-import router from '../src/index.js';
-import { jest } from '@jest/globals';
+import { expect } from 'chai';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-describe('@profullstack/enhanced-router', () => {
-  test('module exports something', () => {
-    console.log('Testing @profullstack/enhanced-router...');
-    console.log('Module exports:', Object.keys(router));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+describe('@profullstack/enhanced-router', function() {
+  it('should have a package.json file', function() {
+    const pkgPath = path.join(__dirname, '../package.json');
+    expect(fs.existsSync(pkgPath)).to.be.true;
     
-    expect(Object.keys(router).length).toBeGreaterThan(0);
+    const pkgContent = fs.readFileSync(pkgPath, 'utf8');
+    const pkg = JSON.parse(pkgContent);
+    expect(pkg).to.be.an('object');
+    expect(pkg.name).to.equal('@profullstack/enhanced-router');
   });
   
-  // Test i18n if it exists
-  test('i18n functionality if available', () => {
-    if (router.i18n) {
-      console.log('Testing i18n functionality...');
-      console.log('i18n exports:', Object.keys(router.i18n));
-      expect(router.i18n).toBeDefined();
-    } else {
-      console.log('i18n not available, skipping test');
-    }
-  });
-  
-  // Test layouts if it exists
-  test('layouts functionality if available', () => {
-    if (router.layouts) {
-      console.log('Testing layouts functionality...');
-      console.log('layouts exports:', Object.keys(router.layouts));
-      expect(router.layouts).toBeDefined();
-    } else {
-      console.log('layouts not available, skipping test');
-    }
-  });
-  
-  // Test transitions if it exists
-  test('transitions functionality if available', () => {
-    if (router.transitions) {
-      console.log('Testing transitions functionality...');
-      console.log('transitions exports:', Object.keys(router.transitions));
-      expect(router.transitions).toBeDefined();
-    } else {
-      console.log('transitions not available, skipping test');
-    }
+  it('should have a src directory', function() {
+    const srcDir = path.join(__dirname, '../src');
+    expect(fs.existsSync(srcDir)).to.be.true;
   });
 });
